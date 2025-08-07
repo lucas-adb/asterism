70aca523daf30e8a4fc2002f54e98d77c484ff8c
lucas-adb <alvesdebrito.lucas@gmail.com>
2025-08-06 21:36:38 -0300

    feat: adds header and main title

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 function App() {
   return (
     <div className="min-h-screen">
-      <h1 className="font-bold text-2xl">Asterism ✨</h1>
-      <p>Bookmark Manager</p>
+      <header className="px-4 py-8 container mx-auto">
+        <h1 className="font-bold text-2xl cursor-pointer">✨ Asterism</h1>
+      </header>
+      <div>
+        <main className="container mx-auto px-4 py-8 text-center">
+          <div className="p-3 border mx-auto rounded-full inline-flex mb-4">
+            <p className="text-sm font-semibold">Version 1.0.b</p>
+          </div>
+          <h1 className="mx-auto text-6xl">
+            Capture the stars of the web. Organize them into your own galaxy.
+          </h1>
+        </main>
+      </div>
     </div>
   );
 }
