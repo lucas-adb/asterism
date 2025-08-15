aeb7a94bde0d79b5610c27fbbf0c66ec8bad0c56
lucas-adb <alvesdebrito.lucas@gmail.com>
2025-08-15 19:08:09 -0300

    feat: adds custom animations

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,7 +5,7 @@ export function Hero() {
         <div className="px-3 py-1 border mx-auto rounded-full inline-flex mb-4">
           <p className="text-sm font-medium">Version 1.0.b</p>
         </div>
-        <h1 className="mx-auto text-5xl md:text-6xl">
+        <h1 className="mx-auto text-5xl md:text-6xl animate-fade-in">
           Capture the stars of the web.
           <br />
           Organize them into your own galaxy.
