3210a60b83dc12241d11d99a96de231b6c7586c2
lucas-adb <alvesdebrito.lucas@gmail.com>
2025-08-30 02:00:25 -0300

    style: adds stars appearing when hovering on text

--- src/components/layout/hero.tsx
+++ src/components/layout/hero.tsx
@@ -6,7 +6,17 @@ export function Hero() {
           <p className="text-sm font-medium">Version 1.0.b</p>
         </div>
         <h1 className="mx-auto text-5xl md:text-6xl animate-fade-in">
-          Capture the stars of the web.
+          Capture the{' '}
+          <span className="relative inline-block group cursor-pointer">
+            <span className="relative">stars</span>
+            <span className="absolute top-3 -left-2 opacity-0 group-hover:opacity-100 transition-all duration-500 ease-out transform group-hover:scale-110 group-hover:-translate-y-1 text-sm">
+              ✨
+            </span>
+            <span className="absolute -bottom-1  -right-4 opacity-0 group-hover:opacity-100 transition-all duration-600 ease-out transform group-hover:scale-110 group-hover:translate-y-1 delay-200 text-3xl">
+              ✨
+            </span>
+          </span>{' '}
+          of the web.
           <br />
           Organize them into your own galaxy.
         </h1>
