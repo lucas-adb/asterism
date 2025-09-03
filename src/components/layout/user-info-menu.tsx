0f107df0124fdc6f509b3c2f89f725af2e1606ef
lucas-adb <alvesdebrito.lucas@gmail.com>
2025-09-03 12:33:52 -0300

    fix: logout when user click on "favorites" inside UserInfoMenu

--- src/components/layout/user-info-menu.tsx
+++ src/components/layout/user-info-menu.tsx
@@ -28,7 +28,6 @@ export function UserInfoMenu() {
           <DropdownMenuLabel>{user?.username}</DropdownMenuLabel>
           <DropdownMenuItem
             onClick={() => {
-              logout();
               navigate('/favorites');
             }}
           >
